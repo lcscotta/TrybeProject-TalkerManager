33a2bd34f0fa11c843b3ebc14470630e9e3e1349
Letícia Cotta <leticiacotta@gmail.com>
2022-06-26 23:45:56 -0300

    Requisito 1. Teste 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,13 +8,47 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const readArchive = async (fileName) => {
+  const archiveJson = await fs.readArchive(fileName, 'utf-8');
+  return JSON.parse(archiveJson);
+};
+
+// Função de gerar os negócios aleatórios nas strings
+function createToken(numchar) {
+  let token = '';
+  const stringSorted = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+  for (let i = 0; i < numchar; i += 1) {
+    const random = parseInt((Math.random() * 36), 0);
+    token = `${token}${(stringSorted[random])}`;
+  }
+
+  return token;
+}
+
+createToken(16);
+
+module.exports = createToken;
+
+app.get('/talker', async (_req, res) => {
+  const palestrantes = await readArchive('./talker.json');
+
+  return res.status(200).json(palestrantes);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const palestrantes = await readArchive('./talker.json');
+  const palestrante = palestrantes.find((acc) => acc.id === Number(id));
+
+  if (!palestrante) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  return res.status(200).json(palestrante);
+});
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
-});
-
-app.get('/talkers', (req, res) => {
-  res.status(200).json({ message: 'Abrindo o PR para o amigo marciano!' });
 });
 
 app.listen(PORT, () => {
